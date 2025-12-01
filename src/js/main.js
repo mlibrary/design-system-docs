7ad75318f3b0de219d5c0facb9b770c38dd1ee0f
Bridget <burkebri@umich.edu>
2025-12-01 10:41:35 -0500

    Package updates and heading anchors

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,19 @@
 let searchBtn = document.querySelector(".pagefind-ui__search-input");
 searchBtn.setAttribute("aria-label", "Search documentation");
 searchBtn.setAttribute("placeholder", "Search documentation");
+
+// Adding link for headings
+document.addEventListener('DOMContentLoaded', () => {
+  // Select headings with IDs
+  document.querySelectorAll('h2[id], h3[id], h4[id], h5[id], h6[id]').forEach(heading => {
+    // Create the link
+    const link = document.createElement('a');
+    link.href = `#${heading.id}`;
+    link.className = 'heading-anchor';
+    link.innerHTML = '#'; 
+    link.setAttribute('aria-label', `Link to ${heading.textContent}`);
+    
+    // Add the link after the heading text
+    heading.appendChild(link);
+  });
+});
